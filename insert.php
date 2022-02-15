1f54ea74a1265850bf0fe0face1d0a143bc8e4a6
gegetapiro <gerardo.trimarco@yahoo.it>
2022-02-15 17:59:32 +0100

    finito sistema inserimento nuovo visitaore

--- insert.php
+++ insert.php
@@ -1,30 +1,41 @@
 <?php
 include "connection.php";
-$date = "02/03/2022";
-$oraingr = "10:20";
-$orausc = "16:45";
+/* $date = "04/03/2022";
+$oraingr = "09:10";
+$orausc = "11:45";
 $greenpass = true;
-$thename = "Giovanni";
-$thesecondname = "Rossi";
-$company = "GinevraS.p.A.";
-$reason = "our goals are to grow up e better society";
-$referent = "John Zippy";
-$targa = "KJ546MN";
+$thename = "Gianni";
+$thesecondname = "Bianchi";
+$company = "Catozzo S.p.A.";
+$reason = "we will do better";
+$referent = "Giancarlo Categani";
+$targa = "NB565MN";
 $note = "disse la vacca al mulo...";
+ */
 
+$date = $_POST["whichdata"];
+$oraingr = $_POST["whenentry"];
+$orausc = $_POST["whenexit"];
+$greenpass = $_POST["passgreen"];
+$thename = $_POST["whichname"];
+$thesecondname = $_POST["whichsurname"];
+$company = $_POST["whichcompany"];
+$reason = $_POST["whichgoal"];
+$referent = $_POST["whoreferent"];
+$targa = $_POST["whichcnumber"];
+$note = $_POST["thenotes"];
 
-if(!$mysqli){
+if (!$mysqli) {
     echo "fails connection";
-}else{
+} else {
     $thequery = "INSERT INTO $table (datagiorno, ora_ingresso, ora_uscita, covidControl, nome, cognome, company, entryReasons, referent, licensePlate, notes)
     VALUES ('$date', '$oraingr', '$orausc', '$greenpass', '$thename',
      '$thesecondname', '$company', '$reason', '$referent', '$targa', '$note')";
 
-     $result = $mysqli->query($thequery);
-     if(!$result){
-         echo "query fallita";
-     }else{
-         echo "succesuful query";
-     }
+    $result = $mysqli->query($thequery);
+    if (!$result) {
+        
+    } else {
+        echo "succesuful query";
+    }
 }
-?>
