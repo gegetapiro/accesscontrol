0b63cd2fdc469846f0875a09b03fc9336243b135
gegetapiro <gerardo.trimarco@yahoo.it>
2022-03-07 14:15:12 +0100

    inserita pagian update.php

--- edit.php
+++ edit.php
@@ -5,17 +5,9 @@ if (!$mysqli) {
     echo "errore di connessione";
 } else {
     $theid = $_POST["whattax"];
-    // $theid = 14;
     $aquery = "SELECT * FROM $table WHERE id = $theid";
     $theresult = $mysqli->query($aquery);
     $row = mysqli_fetch_array($theresult, MYSQLI_ASSOC);
-    /*  $fields = [];
-    for ($i = 0; $i < 11; $i++) {
-        $fields[$i];
-    } */
-
-
-
-
     echo json_encode($row);
 }
+
---
+++ update.php
@@ -0,0 +1,18 @@
+<?php
+include "connection.php";
+if(!$mysqli){
+    echo "connection problem";
+}else{
+    $data = $_POST[""];
+    $entrata = $_POST[""];
+    $uscita = $_POST[""];
+    $greenpass = $_POST[""];
+    $nome = $_POST[""];
+    $cognome = $_POST[""];
+    $azienda = $_POST[""];
+    $motivo = $_POST[""];
+    $referente = $_POST[""];
+    $targa = $_POST[""];
+    $note = $_POST[""];
+    $aquery = ""
+}
