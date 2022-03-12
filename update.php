b260c14253d48f55ed3045648cb0d1f08b33cbd1
gegetapiro <gerardo.trimarco@yahoo.it>
2022-03-12 14:06:42 +0100

    inseriti .val

--- update.php
+++ update.php
@@ -15,12 +15,26 @@ if (!$mysqli) {
     $referente = $_POST["refernt2"];
     $targa = $_POST["countrynum2"];
     $note = $_POST["notes2"];
-    $aquery = "UPDATE $table SET datagiorno = $data, ora_ingresso = $entrata, ora_uscita = $uscita,
-     covidControl = $greenpass, nome = $nome, cognome = $cognome, company = $azienda, entryReasons = $motivo,
-     referent = $referente, licensePlate = $targa, notes = $note WHERE id = $id";
+
+    /* $id = 1;
+    $data = "12/03/2022";
+    $entrata = "20:12";
+    $uscita = "21:30";
+    $greenpass = "no";
+    $nome = "Giorgio";
+    $cognome = "Panariello";
+    $azienda = "Microsoft";
+    $motivo = "Modificare server";
+    $referente = "Giancarlo Siani";
+    $targa = "BBBBBBBBBBB";
+    $note = "Era meglio morire da piccoli"; */
+    $aquery = "UPDATE $table SET datagiorno = '$data', ora_ingresso = '$entrata', ora_uscita = '$uscita',   
+     covidControl = '$greenpass', nome = '$nome', cognome = '$cognome', company = '$azienda', entryReasons = '$motivo',
+     referent = '$referente', licensePlate = '$targa', notes = '$note' WHERE id = $id";
     $result = $mysqli->query($aquery);
     if (!$result) {
-        echo "query fallita";
+        echo "query fallita " . $mysqli->error;
+        exit();
     } else {
         echo "record modificato correttamente";
     }
