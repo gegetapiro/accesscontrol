82cdf7a3fa6819d63eb7711e7d63352d45359ab7
gegetapiro <gerardo.trimarco@yahoo.it>
2022-03-14 19:02:11 +0100

    inseriti altri campi di modifica

--- update.php
+++ update.php
@@ -12,22 +12,24 @@ if (!$mysqli) {
     $cognome = $_POST["surname2"];
     $azienda = $_POST["company2"];
     $motivo = $_POST["goal2"];
-    $referente = $_POST["refernt2"];
+    $referente = $_POST["referent2"];
     $targa = $_POST["countrynum2"];
     $note = $_POST["notes2"];
 
-    /* $id = 1;
+    $/* id = 1;
     $data = "12/03/2022";
     $entrata = "20:12";
     $uscita = "21:30";
     $greenpass = "no";
-    $nome = "Giorgio";
-    $cognome = "Panariello";
-    $azienda = "Microsoft";
+    $nome = "Modificato";
+    $cognome = "Modificato";
+    $azienda = "modificato";
     $motivo = "Modificare server";
-    $referente = "Giancarlo Siani";
-    $targa = "BBBBBBBBBBB";
-    $note = "Era meglio morire da piccoli"; */
+    $referente = "Modificato";
+    $targa = "CCCCCCCCCCCCCCCCCCCC";
+    $note = "Era meglio morire da grandi"; */
+    
+    
     $aquery = "UPDATE $table SET datagiorno = '$data', ora_ingresso = '$entrata', ora_uscita = '$uscita',   
      covidControl = '$greenpass', nome = '$nome', cognome = '$cognome', company = '$azienda', entryReasons = '$motivo',
      referent = '$referente', licensePlate = '$targa', notes = '$note' WHERE id = $id";
