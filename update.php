693ee7bff7b421dd5a004b95e4dec9cd3d237426
gegetapiro <gerardo.trimarco@yahoo.it>
2022-03-25 14:15:04 +0100

    prima di push

--- update.php
+++ update.php
@@ -4,6 +4,7 @@ if (!$mysqli) {
     echo "connection problem";
 } else {
     $id = $_POST["theid"];
+    echo gettype($id);
     $data = $_POST["date2"];
     $entrata = $_POST["entry2"] + ":" + $_POST["entryminutes2"];
     $uscita = $_POST["exit2"] + ":" + $_POST["minutiuscita2"];
@@ -16,23 +17,23 @@ if (!$mysqli) {
     $targa = $_POST["countrynum2"];
     $note = $_POST["notes2"];
 
-    // $id = 24;
-    // $data = "12/03/2022";
-    // $entrata = "20:12";
-    // $uscita = "21:30";
-    // $greenpass = "no";
-    // $nome = "Modificato";
-    // $cognome = "Modificato";
-    // $azienda = "modificato";
-    // $motivo = "Modificare server";
-    // $referente = "Modificato";
-    // $targa = "ttttttttttt";
-    // $note = "barcollo";
+    /*  $id = 2;
+    $data = "12/03/2022";
+    $entrata = "20:12";
+    $uscita = "21:30";
+    $greenpass = "no";
+    $nome = "Edited";
+    $cognome = "Edited";
+    $azienda = "Edited";
+    $motivo = "Modificare server";
+    $referente = "Edited";
+    $targa = "ttttttttttt";
+    $note = "barcollo";
+ */
 
-
-    $aquery = "UPDATE $table SET datagiorno = $data, ora_ingresso = $entrata, ora_uscita = $uscita,   
-     covidControl = $greenpass, nome = $nome, cognome = $cognome, company = $azienda, entryReasons = $motivo,
-     referent = $referente, licensePlate = $targa, notes = $note WHERE id = $id";
+    $aquery = "UPDATE $table SET datagiorno = '$data', ora_ingresso = '$entrata', ora_uscita = '$uscita',   
+     covidControl = '$greenpass', nome = '$nome', cognome = '$cognome', company = '$azienda', entryReasons = '$motivo',
+     referent = '$referente', licensePlate = '$targa', notes = '$note' WHERE id = $id";
     $result = $mysqli->query($aquery);
 
     if (!$result) {
