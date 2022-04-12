bebc2f52b1f73bee68e57d34c775c19706fb60d4
gegetapiro <gerardo.trimarco@yahoo.it>
2022-04-12 17:42:14 +0200

    finito blocco ricerca, va raffinato, usato paradigma one page application

--- searchby.php
+++ searchby.php
@@ -12,11 +12,15 @@ if (!$mysqli) {
         echo "problematic query";
     } else {
         // echo "Returned rows are: " . $result->free_result('nome');
-        while ($row = $result->fetch_array()) {
-            echo $row['datagiorno'] . " " .  $row['ora_ingresso'] . " " .  $row['ora_uscita']
-                . " " .  $row['covidControl']   . " " .  $row['nome'] . " " .  $row['cognome']
-                . " " .  $row['company'] . " " .  $row['entryReasons'] . " " .  $row['referent']
-                . " " .  $row['licensePlate'] . " " .  $row['notes'];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                /*  echo $row['datagiorno'] . " " .  $row['ora_ingresso'] . " " .  $row['ora_uscita']
+                    . " " .  $row['covidControl']   . " " .  $row['nome'] . " " .  $row['cognome']
+                    . " " .  $row['company'] . " " .  $row['entryReasons'] . " " .  $row['referent']
+                    . " " .  $row['licensePlate'] . " " .  $row['notes']; */
+                $rowcontent['visitatori'][] = $row;
+            }
+            echo json_encode($rowcontent);
         }
     }
 }
