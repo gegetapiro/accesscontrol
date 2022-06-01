d16930d3b1ff4157fa62e240009faea2c0ae33f0
gegetapiro <gerardo.trimarco@yahoo.it>
2022-06-01 14:52:06 +0200

    incominciato lavoro inserimento lavoratori da CF

--- barCodeControl/compareCF.php
+++ barCodeControl/compareCF.php
@@ -4,12 +4,21 @@ if (!$mysqli) {
     echo "connessione fallita";
 } else {
     $fiscode = "RNNDCC80A41C895O";
-    $searchquery = "SELECT * FROM $table2 WHERE codiceFiscale = $fiscode";
-   $result = $mysqli->query($searchquery);
-        if($result->num_rows > 0){
-                while($row = $result->fetch_array(MYSQLI_ASSOC)){
-                    echo $row;
-                }
+    $searchquery = "SELECT * FROM $table2 WHERE codiceFiscale = '$fiscode'";
+    $result = $mysqli->query($searchquery);
+    if ($result->num_rows > 0) {
+        // echo "file trovato";
+        $elem = array();
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            array_push($elem, $row['nome'], $row['cognome'], $row['azienda'], $row['note']);
         }
-    
+        // echo $elem[0];
+        // echo date('d, m, yyyy') . "cazzi";
+        //    echo  date('d-m-Y H:i:s', strtotime('+1 year', strtotime($today)) );
+        echo date('H:m');
+        $datatime =  date('d/m/Y');
+        $time = date('h:m');
+        // $insertquery = "INSERT * FROM $table (datagiorno, ora_ingresso, nome, cognome, company) VALUE ()"
+        // $mysqli->close();
+    }
 }
