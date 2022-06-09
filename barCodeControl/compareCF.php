c87bb324a5bb4baab4590c8838e27acb2ef41c97
gegetapiro <gerardo.trimarco@yahoo.it>
2022-06-09 17:27:34 +0200

    commit per step back

--- barCodeControl/compareCF.php
+++ barCodeControl/compareCF.php
@@ -7,24 +7,31 @@ if (!$mysqli) {
     $searchquery = "SELECT * FROM $table2 WHERE codiceFiscale = '$employe'";
     $result = $mysqli->query($searchquery);
     if ($result->num_rows > 0) {
-        // echo "file trovato";
+        // echo "file trovato posi 1";
         $elem = array();
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
             array_push($elem, $row['datagiorno'], $row['nome'], $row['cognome'], $row['azienda'], $row['note']);
         }
-        /* $elem[0] = '04/03/2022';
-        $elem[1] = 'Gianni';
-        $elem[2] = 'Bianchi'; */
+
         // ############# CONTROLLO PRESENZA LAVORATORE O PRIMO ARRIVO #############
         $datatime =  date('d/m/Y');
         $time = date('H:i');
-        $checkquery = "SELECT ora_ingresso FROM `accesses` WHERE datagiorno 
-         = '$datatime' AND nome = '$elem[1]' AND cognome = '$elem[2]'";
-        $resultx = $mysqli->query($checkquery);
+        $checkquery = "SELECT * FROM $table WHERE datagiorno = '$datatime' AND nome = '$elem[1]' AND cognome = '$elem[2]'";
+        $resultx2 = $mysqli->query($checkquery);
+        $elemcheck = array();
+        if ($resultx2->num_rows > 0) {
+            while ($rowcheck = $resultx2->fetch_array(MYSQLI_ASSOC)) {
+                array_push($elemcheck, $rowcheck['ora_ingresso']);
+            }
+        }
+        echo "QUI " . $elemcheck[0];
+
         if ($resultx->num_rows > 0) {
-            $queryinssertexit = "INSERT INTO $table (ora_uscita) VALUE ('$time') ";
-          /*   ####### qui ######### */
+            echo "file trovato posi 2";
+            /*  ############## vuol dire che il lavoratore è presente e sta uscendo ############### */
+            $queryinssertexit = "INSERT INTO $table (ora_uscita) VALUE ('$time') WHERE nome = $elem[1] AND cognome = $elem[2]";
         } else {
+            /* ################## vuo dire che il lavoratore sta entrando per la prima volta nella giornata ########### */
             $insertquery = "INSERT INTO $table (datagiorno, ora_ingresso, nome, cognome, company)
             VALUE ('$datatime', '$time', '$elem[0]', '$elem[1]', '$elem[2]')";
             $result_2 = $mysqli->query($insertquery);
