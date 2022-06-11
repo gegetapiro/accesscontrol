df530c67678a1c58b84a70feea1f5bf940a17210
gegetapiro <gerardo.trimarco@yahoo.it>
2022-06-11 11:33:36 +0200

    commit per continuare lavoro da pc desktop a casa

--- barCodeControl/compareCF.php
+++ barCodeControl/compareCF.php
@@ -3,44 +3,65 @@ include "../connection.php";
 if (!$mysqli) {
     echo "connessione fallita";
 } else {
+
     $employe = $_POST["codfisc"];
     $searchquery = "SELECT * FROM $table2 WHERE codiceFiscale = '$employe'";
     $result = $mysqli->query($searchquery);
+
     if ($result->num_rows > 0) {
-        // echo "file trovato posi 1";
+
         $elem = array();
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            array_push($elem, $row['datagiorno'], $row['nome'], $row['cognome'], $row['azienda'], $row['note']);
+            array_push($elem, $row['nome'], $row['cognome'], $row['azienda'], $row['note']);
         }
 
         // ############# CONTROLLO PRESENZA LAVORATORE O PRIMO ARRIVO #############
         $datatime =  date('d/m/Y');
+
         $time = date('H:i');
-        $checkquery = "SELECT * FROM $table WHERE datagiorno = '$datatime' AND nome = '$elem[1]' AND cognome = '$elem[2]'";
-        $resultx2 = $mysqli->query($checkquery);
+
+        $checkquery = "SELECT * FROM $table WHERE datagiorno = '$datatime' AND nome = '$elem[0]' AND cognome = '$elem[1]'";
+
         $elemcheck = array();
-        if ($resultx2->num_rows > 0) {
+
+        $resultx2 = $mysqli->query($checkquery);
+
+        if ($resultx2) {
+
+            echo "fino a qui va " . $elem[1] . PHP_EOL;
+
             while ($rowcheck = $resultx2->fetch_array(MYSQLI_ASSOC)) {
                 array_push($elemcheck, $rowcheck['ora_ingresso']);
             }
-        }
-        echo "QUI " . $elemcheck[0];
 
-        if ($resultx->num_rows > 0) {
-            echo "file trovato posi 2";
-            /*  ############## vuol dire che il lavoratore è presente e sta uscendo ############### */
-            $queryinssertexit = "INSERT INTO $table (ora_uscita) VALUE ('$time') WHERE nome = $elem[1] AND cognome = $elem[2]";
-        } else {
-            /* ################## vuo dire che il lavoratore sta entrando per la prima volta nella giornata ########### */
-            $insertquery = "INSERT INTO $table (datagiorno, ora_ingresso, nome, cognome, company)
-            VALUE ('$datatime', '$time', '$elem[0]', '$elem[1]', '$elem[2]')";
-            $result_2 = $mysqli->query($insertquery);
-            if (!$result_2) {
-                echo "query fallita " . $mysqli->error;
+            if ($elemcheck[0] == null) {
+                echo $elemcheck[0] . PHP_EOL;
+                echo "primo accesso del giorno " . PHP_EOL;
+                $insertquery = "INSERT INTO $table (datagiorno, ora_ingresso, nome, cognome, company)
+                VALUE ('$datatime', '$time', '$elem[0]', '$elem[1]', '$elem[2]')";
+                $result_2 = $mysqli->query($insertquery);
+                if (!$result_2) {
+                    echo "query fallita " . $mysqli->error;
+                } else {
+                    echo "lavoratore " . $elem[0] . " " . $elem[1] . " inserito correttamente IN ENTRATA";
+                }
             } else {
-                echo "lavoratore " . $elem[0] . " " . $elem[1] . " inserito correttamente IN ENTRATA";
+
+                /*  ############## vuol dire che il lavoratore è presente e sta uscendo ############### */
+                $queryinssertexit = "UPDATE $table SET ora_uscita = '$time' WHERE nome = '$elem[1]' AND cognome = '$elem[2]'";
+                echo "lavoratore in uscita tabella = " . getType($time) . PHP_EOL;
+                // UPDATE accesses SET ora_uscita = '11:11' WHERE nome = 'Alessandro' AND cognome = 'Meloni'
+                $result_3 = $mysqli->query($queryinssertexit);
+                if (!$result_3) {
+                    echo "fallito inserimento ora uscita";
+                } else {
+                    echo "lavoratore " . $elem[1] . " " . $elem[2] . " ha timbrato l'uscita alle ore " . $time;
+                }
             }
         }
+
+
+
 
         // #########################################################################
 
