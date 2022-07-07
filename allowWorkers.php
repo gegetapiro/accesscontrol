4efeb448cb2a892aa079107be3e2a985282a971b
gegetapiro <gerardo.trimarco@yahoo.it>
2022-07-07 17:43:25 +0200

    aggiunta visualizzazione lavoratori ammessi

---
+++ allowWorkers.php
@@ -0,0 +1,20 @@
+<?php
+include "connection.php";
+if (!$mysqli) {
+    echo "connessione falita";
+} else {
+    if ($mysqli->connect_error) {
+        die('Errore di connessione (' . $result->connect_errno . ') ' . $result->connect_error);
+    } else {
+        $allowsquery = "SELECT * FROM $table2  ORDER BY `id` DESC";
+        $resulallows = mysqli_query($mysqli, $allowsquery);
+        if ($resulallows->num_rows > 0) {
+            while ($allowrow = $resulallows->fetch_array(MYSQLI_ASSOC)) {
+                $element['ammessi'][] = $allowrow;
+            }
+            echo json_encode($element);
+        } else {
+            echo "la tabella è vuota";
+        }
+    }
+}
