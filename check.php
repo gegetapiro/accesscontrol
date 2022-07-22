e9695277652097f4ca7282c0e237d69da46dd58c
gegetapiro <gerardo.trimarco@yahoo.it>
2022-07-22 17:43:20 +0200

    continuata impllementazione controllo con variabili di sessione

--- check.php
+++ check.php
@@ -1,25 +1,28 @@
 <?php
+session_start();
 include "connection.php";
 if (!$mysqli) {
     echo "connessione fallita";
 } else {
     $themail = $_POST["email"];
     $password = $_POST["pw"];
-    $querylogin = "SELECT * FROM $table3";
+    $querylogin = "SELECT email, password FROM $table3 WHERE email = '$themail' LIMIT 1";
     if ($resultlogin = $mysqli->query($querylogin)) {
-        if ($resultlogin->num_rows > 0) {
-            while ($rows = $resultlogin->fetch_array(MYSQLI_ASSOC)) {
-                $element['observers_modifiers'][] = $rows;
-                if ($themail == $element['observers_modifiers']['email'] and $password == $element['observers_modifiers']['email']) {
-                    echo "utente trovato";
-                } else {
-                    echo "utente assente";
-                }
-            }
-            echo json_encode($element);
+        if ($resultlogin->num_rows != 1) {
+            die("utente non trovato");
         }
-        // foreach()
+        while ($user = $resultlogin->fetch_assoc()) {
+            if ($user['password'] != $password) {
+                die("password errata");
+            }
+        }
     } else {
         echo "query fallita";
     }
+
+    echo ("user trovato");
+    $_SESSION['usersession'] = $user;
 }
+
+/* ############### view this: https://www.php.net/manual/en/reserved.variables.session.php ##########
+ */
